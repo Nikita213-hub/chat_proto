2ac1dd343897e4af0cbcbc1f7186cfd216012435
Nikita213-hub <nikitaspirin2603@gmail.com>
2025-02-12 23:30:15 +0300

    wrapper message added

--- communicationEntities.pb.go
+++ communicationEntities.pb.go
@@ -161,6 +161,104 @@ func (x *Notification) GetContent() string {
 	return ""
 }
 
+type WrapperMessage struct {
+	state protoimpl.MessageState `protogen:"open.v1"`
+	// Types that are valid to be assigned to Msg:
+	//
+	//	*WrapperMessage_Cm
+	//	*WrapperMessage_Em
+	//	*WrapperMessage_Nm
+	Msg           isWrapperMessage_Msg `protobuf_oneof:"msg"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *WrapperMessage) Reset() {
+	*x = WrapperMessage{}
+	mi := &file_communicationEntities_proto_msgTypes[3]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *WrapperMessage) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*WrapperMessage) ProtoMessage() {}
+
+func (x *WrapperMessage) ProtoReflect() protoreflect.Message {
+	mi := &file_communicationEntities_proto_msgTypes[3]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use WrapperMessage.ProtoReflect.Descriptor instead.
+func (*WrapperMessage) Descriptor() ([]byte, []int) {
+	return file_communicationEntities_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *WrapperMessage) GetMsg() isWrapperMessage_Msg {
+	if x != nil {
+		return x.Msg
+	}
+	return nil
+}
+
+func (x *WrapperMessage) GetCm() *ChatMessage {
+	if x != nil {
+		if x, ok := x.Msg.(*WrapperMessage_Cm); ok {
+			return x.Cm
+		}
+	}
+	return nil
+}
+
+func (x *WrapperMessage) GetEm() *ErrorMessage {
+	if x != nil {
+		if x, ok := x.Msg.(*WrapperMessage_Em); ok {
+			return x.Em
+		}
+	}
+	return nil
+}
+
+func (x *WrapperMessage) GetNm() *Notification {
+	if x != nil {
+		if x, ok := x.Msg.(*WrapperMessage_Nm); ok {
+			return x.Nm
+		}
+	}
+	return nil
+}
+
+type isWrapperMessage_Msg interface {
+	isWrapperMessage_Msg()
+}
+
+type WrapperMessage_Cm struct {
+	Cm *ChatMessage `protobuf:"bytes,1,opt,name=cm,proto3,oneof"`
+}
+
+type WrapperMessage_Em struct {
+	Em *ErrorMessage `protobuf:"bytes,2,opt,name=em,proto3,oneof"`
+}
+
+type WrapperMessage_Nm struct {
+	Nm *Notification `protobuf:"bytes,3,opt,name=nm,proto3,oneof"`
+}
+
+func (*WrapperMessage_Cm) isWrapperMessage_Msg() {}
+
+func (*WrapperMessage_Em) isWrapperMessage_Msg() {}
+
+func (*WrapperMessage_Nm) isWrapperMessage_Msg() {}
+
 var File_communicationEntities_proto protoreflect.FileDescriptor
 
 var file_communicationEntities_proto_rawDesc = string([]byte{
@@ -176,9 +274,20 @@ var file_communicationEntities_proto_rawDesc = string([]byte{
 	0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74, 0x22,
 	0x28, 0x0a, 0x0c, 0x4e, 0x6f, 0x74, 0x69, 0x66, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x12,
 	0x18, 0x0a, 0x07, 0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
-	0x52, 0x07, 0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74, 0x42, 0x0e, 0x5a, 0x0c, 0x2e, 0x2f, 0x63,
-	0x68, 0x61, 0x74, 0x5f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x33,
+	0x52, 0x07, 0x63, 0x6f, 0x6e, 0x74, 0x65, 0x6e, 0x74, 0x22, 0xbb, 0x01, 0x0a, 0x0e, 0x57, 0x72,
+	0x61, 0x70, 0x70, 0x65, 0x72, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x34, 0x0a, 0x02,
+	0x63, 0x6d, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x22, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x75,
+	0x6e, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x45, 0x6e, 0x74, 0x69, 0x74, 0x69, 0x65, 0x73,
+	0x2e, 0x43, 0x68, 0x61, 0x74, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x48, 0x00, 0x52, 0x02,
+	0x63, 0x6d, 0x12, 0x35, 0x0a, 0x02, 0x65, 0x6d, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x23,
+	0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x75, 0x6e, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x45, 0x6e,
+	0x74, 0x69, 0x74, 0x69, 0x65, 0x73, 0x2e, 0x45, 0x72, 0x72, 0x6f, 0x72, 0x4d, 0x65, 0x73, 0x73,
+	0x61, 0x67, 0x65, 0x48, 0x00, 0x52, 0x02, 0x65, 0x6d, 0x12, 0x35, 0x0a, 0x02, 0x6e, 0x6d, 0x18,
+	0x03, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x23, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x75, 0x6e, 0x69, 0x63,
+	0x61, 0x74, 0x69, 0x6f, 0x6e, 0x45, 0x6e, 0x74, 0x69, 0x74, 0x69, 0x65, 0x73, 0x2e, 0x4e, 0x6f,
+	0x74, 0x69, 0x66, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x48, 0x00, 0x52, 0x02, 0x6e, 0x6d,
+	0x42, 0x05, 0x0a, 0x03, 0x6d, 0x73, 0x67, 0x42, 0x0e, 0x5a, 0x0c, 0x2e, 0x2f, 0x63, 0x68, 0x61,
+	0x74, 0x5f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 })
 
 var (
@@ -193,18 +302,22 @@ func file_communicationEntities_proto_rawDescGZIP() []byte {
 	return file_communicationEntities_proto_rawDescData
 }
 
-var file_communicationEntities_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_communicationEntities_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_communicationEntities_proto_goTypes = []any{
-	(*ChatMessage)(nil),  // 0: communicationEntities.ChatMessage
-	(*ErrorMessage)(nil), // 1: communicationEntities.ErrorMessage
-	(*Notification)(nil), // 2: communicationEntities.Notification
+	(*ChatMessage)(nil),    // 0: communicationEntities.ChatMessage
+	(*ErrorMessage)(nil),   // 1: communicationEntities.ErrorMessage
+	(*Notification)(nil),   // 2: communicationEntities.Notification
+	(*WrapperMessage)(nil), // 3: communicationEntities.WrapperMessage
 }
 var file_communicationEntities_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	0, // 0: communicationEntities.WrapperMessage.cm:type_name -> communicationEntities.ChatMessage
+	1, // 1: communicationEntities.WrapperMessage.em:type_name -> communicationEntities.ErrorMessage
+	2, // 2: communicationEntities.WrapperMessage.nm:type_name -> communicationEntities.Notification
+	3, // [3:3] is the sub-list for method output_type
+	3, // [3:3] is the sub-list for method input_type
+	3, // [3:3] is the sub-list for extension type_name
+	3, // [3:3] is the sub-list for extension extendee
+	0, // [0:3] is the sub-list for field type_name
 }
 
 func init() { file_communicationEntities_proto_init() }
@@ -212,13 +325,18 @@ func file_communicationEntities_proto_init() {
 	if File_communicationEntities_proto != nil {
 		return
 	}
+	file_communicationEntities_proto_msgTypes[3].OneofWrappers = []any{
+		(*WrapperMessage_Cm)(nil),
+		(*WrapperMessage_Em)(nil),
+		(*WrapperMessage_Nm)(nil),
+	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_communicationEntities_proto_rawDesc), len(file_communicationEntities_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
